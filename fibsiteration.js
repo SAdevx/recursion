0a7bedceed90330dbd46081ae88602b1764ef216
CStalks <swiafe.dev@gmail.com>
2024-07-05 20:51:03 -0400

    Solve fibonacci iterative version

---
+++ fibsiteration.js
@@ -0,0 +1,20 @@
+function fibs(num){
+    let arr = [];
+
+    if(num < 0) return [];
+    
+    if(num >= 0)  arr.push(0);
+    if(num >= 1)  arr.push(1);
+
+    for(let i = 2; i < num; i++){
+        let sum = arr[i - 1] + arr[i - 2]
+        arr.push(sum);
+    }
+    return arr;
+}
+
+console.log(fibs(8));
+console.log(fibs(6));
+console.log(fibs(10));
+console.log(fibs(50));
+console.log(fibs(-3));
